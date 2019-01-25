35a6877d2fdcbd04bba7e69d593492fb9f972185
Demiurgis <bshipos@gmail.com>
2019-01-25 15:48:24 -0800

    Updates

---
+++ precache-manifest.50f2e3043e76e6ca58816894c113f15d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab1544e222147bf60b85be756e018ed2",
+    "url": "/cat-friends/static/media/Brown Choco.ab1544e2.woff"
+  },
+  {
+    "revision": "1fffe9481e5d9a33476e",
+    "url": "/cat-friends/static/js/runtime~main.1fffe948.js"
+  },
+  {
+    "revision": "5c3ea4b253ede258efda",
+    "url": "/cat-friends/static/js/main.5c3ea4b2.chunk.js"
+  },
+  {
+    "revision": "dbcf4ff0c4f839adb66e",
+    "url": "/cat-friends/static/js/1.dbcf4ff0.chunk.js"
+  },
+  {
+    "revision": "5c3ea4b253ede258efda",
+    "url": "/cat-friends/static/css/main.9f008225.chunk.css"
+  },
+  {
+    "revision": "dbcf4ff0c4f839adb66e",
+    "url": "/cat-friends/static/css/1.5cbc2147.chunk.css"
+  },
+  {
+    "revision": "68f92cdff570113eea52a0a5fe1ec031",
+    "url": "/cat-friends/index.html"
+  }
+];
