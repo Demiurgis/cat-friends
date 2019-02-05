8a3520db8b072970c3fb1e5c4224af30224e6cc0
Demiurgis <bshipos@gmail.com>
2019-02-05 14:01:17 -0800

    Modifying app to use redux for state management

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
-import { cats } from  '../cats';
+// import { cats } from  '../cats';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
+import { setSearchField } from '../actions';
+import { searchCats } from '../reducers';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchCats
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
 			cats: [],
-			searchfield: ''
 		}
 	}
 
 	componentDidMount(){
 		fetch('https://jsonplaceholder.typicode.com/users')
   		.then(response => response.json())
-  		.then(users => this.setState({ cats: cats }));
-	}
-
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
+  		.then(users => this.setState({ cats: users }));
 	}
 
 	render() {
-		const { cats, searchfield } = this.state;
+		const { cats } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredCats = cats.filter(cat => {
-			return cat.name.toLowerCase().includes(searchfield.toLowerCase());
+			return cat.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 		return !cats.length ?  
 			<h1 className='catTitle'>Loading</h1> :
 			(
 				<div className='tc'>
 					<h1 className='catTitle'>Cat Friends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
+					<SearchBox searchChange={ onSearchChange }/>
 					<Scroll>
 						<ErrorBoundry>
 							<CardList cats={ filteredCats } />
@@ -46,4 +57,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
+import { searchCats } from './reducers';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />	, document.getElementById('root'));
+const store = createStore(searchCats);
 
+ReactDOM.render(
+	<Provider store= { store }>
+		<App />	
+	</Provider>, document.getElementById('root'));
+	
 serviceWorker.unregister();
